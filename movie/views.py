cc7625ef2de830fd39e1f2c1145bfa95d4c12473
tunmie <victoroluwasomidotun@gmail.com>
2022-08-12 02:37:32 +0100

    news done

--- movie/views.py
+++ movie/views.py
@@ -6,6 +6,10 @@ from movie.models import Movie
 
 def home(request):
 	searchTerm = request.GET.get('searchMovie')
+	if searchTerm:
+		movies = Movie.objects.filter(title__icontains=searchTerm)
+	else:
+		movies = Movie.objects.all()
 	movies = Movie.objects.all()
 	return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
 
--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from movie import views as movieViews
 
@@ -26,6 +26,7 @@ urlpatterns = [
 	path('', movieViews.home, name='home'),
 	path('about/', movieViews.about, name='about'),
 	path('signup/', movieViews.signup, name='signup'),
+	path('news/', include('news.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
